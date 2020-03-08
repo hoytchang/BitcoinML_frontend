ade142f41e2e2f9215a5bd2e3ca315abb3e56c8c
Hoyt Chang <hoyt.chang@gmail.com>
2020-03-08 16:45:21 -0400

    add bitcoin price chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,54 +1,31 @@
 import React, {Component} from 'react';
-import {Bar, Line, Pie} from 'react-chartjs-2';
+import {Scatter} from 'react-chartjs-2';
 
 class Chart extends Component{
     constructor(props){
         super(props);
         this.state = {
-            chartData:{
-                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Combridge', 'New Bedford'],
-                datasets:[
-                    {
-                        label: 'Population',
-                        data:[
-                            617594,
-                            181045,
-                            153060,
-                            106519,
-                            105162,
-                            95071
-                        ],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.6)',
-                            'rgba(54, 162, 235, 0.6)',
-                            'rgba(255, 206, 86, 0.6)',
-                            'rgba(75, 192, 192, 0.6)',
-                            'rgba(153, 102, 255, 0.6)',
-                            'rgba(255, 159, 64, 0.6)',
-                            'rgba(255, 99, 132, 0.6)',
-                        ]
-                    }
-                ]
-            }
+            chartData:props.chartData
         }
     }
 
     static defaultProps = {
         displayTitle: true,
         displayLegend: true,
-        legendPosition: 'right'
+        legendPosition: 'right',
+        title: ''
     }
     
     render(){
         return(
             <div className="chart">
-                <Bar
+                <Scatter
                     data={this.state.chartData}
                     options={{
-                        maintainAspectRatio:true,
+                        maintainAspectRatio:false,
                         title:{
                             display:this.props.displayTitle,
-                            text:'Largest Cities in Massachusetts',
+                            text:this.props.title,
                             fontSize:25,
                         },
                         legend:{
@@ -56,7 +33,8 @@ class Chart extends Component{
                             position:this.props.legendPosition,
                         }
                     }}
-                    height="60"
+                    height="400"
+                    //width="200"
                 />
             </div>
         )
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import Chart from '../components/Chart'
+import { Layout, Row, Col } from 'antd';
+import bitcoinPrice from '../data/market-price.json';
 
 class Main extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            priceData:{},
+        };
+    }
+
+    componentWillMount(){
+        this.getChartData();
+    }
+
+    getChartData(){
+        // ajax call here
+        this.setState({
+            priceData: {
+                labels: ['price'],
+                datasets:[
+                    {
+                        label: 'Price',
+                        showLine: true,
+                        pointRadius: 0.0,
+                        data: bitcoinPrice.values,
+                        //backgroundColor: 'red'
+                    }
+                ]
+            },
+        });
+    }
+
     render() {
         return (
             <div>
-                <Chart legendPosition='left' />
+                <div style={{}}>
+                    <div style={{display:"inline", background: 'lightgrey'}}>
+                        left
+                        
+                    </div>
+                    <div style={{display:"inline", background: 'black', color:"white"}}>
+                        mid
+                        
+                    </div>
+                    <div style={{display:"inline", background: 'grey'}}>
+                        right
+                    </div>
+                </div>
+                <div 
+                    //style={{height:"400px", width:"800px"}}
+                >
+                    <Chart
+                        chartData={this.state.priceData}
+                        title='Bitcoin Market Price (USD)'
+                    />
+
+                </div>
             </div>
         );
     }
